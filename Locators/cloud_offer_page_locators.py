a40a10427be81a9a6e7f8e877a423c29b1cf99d3
guy.oknin <guyokps@gmail.com>
2025-01-05 13:06:54 +0200

    Adding Locators file, adding html report to yml file

---
+++ Locators/cloud_offer_page_locators.py
@@ -0,0 +1,13 @@
+class OfferPageLocators:
+    hitman_url_offer_page = 'https://playable.ludeo.com/8774c270-e3b0-4ef2-ae32-5fda703574bc/offer'
+    hitman_xbox_shop_url = 'https://www.xbox.com/en-us/games/store/hitman-world-of-assassination-free-starter-pack/9nvjwr50krxx?utm_source=ludeo&utm_medium=direct'
+    xbox_shop_visibility = 'XBoxPLAY'
+
+
+
+    # Actions
+    play_again_btn = 'page.get_by_role("button", name="Play again")'
+
+
+    # expected_url = OfferPageLocators.hitman_url_offer_page
+    # expect(page).to_have_url(expected_url)
--- offer_page_tests/test_xbox_shop.py
+++ offer_page_tests/test_xbox_shop.py
@@ -1,4 +1,7 @@
+import re
+
 from playwright.sync_api import Page, expect
+from Locators.cloud_offer_page_locators import OfferPageLocators
 
 
 def test_xbox_shop_visibility(page: Page) -> None:
@@ -7,3 +10,20 @@ def test_xbox_shop_visibility(page: Page) -> None:
     expect(page.locator("#root")).to_contain_text("PLAY")
 
 # Add navigation to shop
+def test_xbox_shop_navigation(page: Page) -> None:
+    # Workaround to avoid offer page bug
+    page.goto(OfferPageLocators.hitman_url_offer_page)
+    page.locator('button', has_text="Play again").click()
+    page.go_back()
+    print(page.url)
+    page.locator("div").filter(has_text=re.compile(r"^XBoxPLAY$")).get_by_role("button").click()
+
+    # The actual test after the workaround
+    # with page.expect_popup() as page1_info:
+    #     page.locator("div").filter(has_text=re.compile(r"^XBoxPLAY$")).get_by_role("button").click()
+    # page1 = page1_info.value
+    # expect(page1.get_by_label("Microsoft", exact=True)).to_be_visible()
+
+
+
+
