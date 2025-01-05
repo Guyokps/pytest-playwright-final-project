8d30be82536c25fdac1e9121cc7a7797b1babea4
guy.oknin <guyokps@gmail.com>
2025-01-05 09:51:56 +0200

    added a html report to each run

--- offer_page_tests/test_discord_link.py
+++ offer_page_tests/test_discord_link.py
@@ -6,7 +6,7 @@ def test_discord_text_and_visibility(page: Page) -> None:
     expect(page.get_by_role("button", name="Any trouble running this game?")).to_be_visible()
     expect(page.locator("#root")).to_contain_text("Any trouble running this game?")
 
-
+# This test will fail if ran in local machine that have Discord installed. should run at cloud
 def test_discord_button_functionality(page: Page) -> None:
     page.goto("https://playable.ludeo.com/8774c270-e3b0-4ef2-ae32-5fda703574bc/offer")
     with page.expect_popup() as page2_info:
